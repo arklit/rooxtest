10cd5360d8cb73b3cf22331f32e75d916da2450d
Misty <artemoutside@gmail.com>
2022-04-16 15:04:12 +0300

    delete garbage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,12 +5,8 @@ import SortBar from '../SortBar/SortBar';
 import Profile from '../Profile/Profile';
 import {IUser} from '../../types/types'
 import axios from 'axios';
-type Sort = {
-  sort: Sort
-}
 function App() {
   const [users, setUsers] = React.useState<IUser[]>([])
-  const [selectedSort, setSelectedSort] = React.useState('')
   async function getUsers() {
     try {
       const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
@@ -19,29 +15,6 @@ function App() {
       console.log(e)
     }
   }
-  // function sortCityUsers() {
-  //   users.sort(function (a, b) {
-  //     if (a.address.city > b.address.city) {
-  //       return 1
-  //     }
-  //     if (a.address.city < b.address.city) {
-  //       return -1;
-  //     }
-  //     return 0
-  //   })
-  // }
-  // function sortCompanyUsers() {
-  //   const draftList= [...users]
-  //   draftList.sort(function (a, b) {
-  //     if (a.company.name > b.company.name) {
-  //       return 1
-  //     }
-  //     if (a.company.name < b.company.name) {
-  //       return -1;
-  //     }
-  //     return 0
-  //   })
-  // }
   const handleSort = (type: any) => {
     const draftList = [...users];
     switch (type) {
